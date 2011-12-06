4c93ffa7c1a6bab446cdd1e71cc47a3ce3d0c942
Ashley Pope <irashp@gmail.com>
2011-12-07 10:59:40 +1100

    read json from a file for blog entrys

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,9 @@
  */
 
 exports.index = function(req, res){
-  var get_blogs = [
-    { heading: "Article #1", content: "Article contents here", posted_by: "jimbob" },
-    { heading: "Fish noms", content: "I like to eat fish </not>", posted_by: "fish lover" },
-    { heading: "JavaScript blog", content: "Javascript hoooooeee!", posted_by: "coffeemang" },
-    { heading: "JavaScript blog", content: "Javascript hoooooeee!", posted_by: "coffeemang" },
-    { heading: "JavaScript blog", content: "Javascript hoooooeee!", posted_by: "coffeemang" },
-    { heading: "JavaScript blog", content: "Javascript hoooooeee!", posted_by: "coffeemang" },
-    { heading: "JavaScript blog", content: "Javascript hoooooeee!", posted_by: "coffeemang" },
-    { heading: "JavaScript blog", content: "Javascript hoooooeee!", posted_by: "coffeemang" },
-  ];
-  res.render('index', { title: 'tiwik: things I wish I knew', blogs: get_blogs })
+  var fs        = require('fs');
+  fs.readFile('public/blogs.json', function(err, data) {
+      if (err) throw err;
+      res.render('index', { title: 'titlehere', blogs: JSON.parse(data) })
+  });
 };
