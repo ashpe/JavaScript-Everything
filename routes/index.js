16fd36313409efc2b831746e8bbbfe4fe35ba1cf
ashpe <irashp@gmail.com>
2011-12-19 22:06:26 +1100

    Updated look of blog. Looks perdy now.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,6 @@ exports.index = function(req, res){
   var fs        = require('fs');
   fs.readFile('public/blogs.json', function(err, data) {
       if (err) throw err;
-      res.render('index', { title: 'titlehere', blogs: JSON.parse(data) })
+      res.render('index', { title: 'index', blogs: JSON.parse(data) } )
   });
 };
